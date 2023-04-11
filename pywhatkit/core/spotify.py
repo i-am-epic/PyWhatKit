b37354a581bf85ffc549c2bd05b3d759c963b3a4
NIKHIL A V <58301643+i-am-epic@users.noreply.github.com>
2023-04-11 23:34:36 +0530

    Update spotify.py

--- pywhatkit/core/spotify.py
+++ pywhatkit/core/spotify.py
@@ -4,14 +4,14 @@ from spotipy.oauth2 import SpotifyOAuth
 import json
 import pprint
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="2a11bcf59a0d42b9ac76a6164ca69301",
-                                                           client_secret="c9d64682ede34421926d3bd159c9472d",redirect_uri="https://web.whatsapp.com/#",
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="hmm",
+                                                           client_secret="winkwink",redirect_uri="https://web.whatsapp.com/#",
                                                scope="user-library-read"))
 
 #results = sp.current_user_playing_track()
 
 SPOTIFY_GET_CURRENT_TRACK_URL = 'https://api.spotify.com/v1/me/player/currently-playing'
-SPOTIFY_ACCESS_TOKEN = 'BQBZWLwp9nIfVTbWtIcsC5AzCSdQvJm7gi0kmod0jfYMTG_flv8BXDnE3DQMnmaSsIdXyQWC8-67JuIS3HSdR6wHlRSraSxa-xUpFfqd7tw5lujwqZxhol8tn2jY9YDxG9t43fMZr-2BfrJY2JsJ6MfCO86gAe-7JMbm9GlrpGpoR1W2_bXYSYZ9cL9RdK5TOHxvdhf2K7zScA'
+SPOTIFY_ACCESS_TOKEN = 'si'
 
 
 
